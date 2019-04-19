78568b5d932f5759855950c00d7b9913aa49185d
Cola Cheng <colacheng@exosite.com>
2019-04-19 23:46:36 +0800

    Move rateLimit to helper.

--- app.js
+++ app.js
@@ -2,41 +2,18 @@ const Config = require('config');
 const path = require('path');
 const express = require('express');
 const app = express();
-const Cache = require('./lib/cache');
-const requestIp = require('request-ip');
-const maxPerWindow = Config.get('maxPerWindow');
 const port = Config.get('port');
 const windowMs = Config.get('windowMs');
-const limitCache = new Cache(windowMs);
+const maxPerWindow = Config.get('maxPerWindow');
+const RateLimit = require('./lib/rateLimit');
+const rateLimiter = RateLimit({windowMs, maxPerWindow});
+const requestIp = require('request-ip');
+
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-function rateLimit(req, res, next) {
-  const ip = req.clientIp;
-  if (!limitCache.hits[ip] || limitCache.hits[ip] < maxPerWindow) {
-    limitCache.increment(ip);
-    res.usage = limitCache.hits[ip];
-    next();
-  } else {
-    res.status(429);
-    res.format({
-      html: _ => {
-        res.render('429', { ip })
-      },
-      json: _ => {
-        res.json({ error: 'Too Many Requests' })
-      },
-      default: _ => {
-        res.type('txt').send('Too Many Requests')
-      }
-    });
-    next();
-  }
-}
-
 app.use(requestIp.mw());
-app.use(rateLimit);
+app.use(rateLimiter);
 
 app.all('/', (req, res) => {
   if (res.statusCode == 200) {
